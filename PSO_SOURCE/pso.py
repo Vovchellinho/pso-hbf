2a099fe64dde4e0de8bb22d71d2d7fc2b69410f1
vovhcellinho <ot4ertsov@gmail.com>
2024-05-09 17:11:03 +0300

    finish pso-hbf algorithm

--- PSO_SOURCE/pso.py
+++ PSO_SOURCE/pso.py
@@ -2,140 +2,140 @@ import numpy as np
 import random
 
 def sphere_function(x):
-    """A simple sphere function."""
-    return np.sum((x - 0.345) ** 2)
+	"""A simple sphere function."""
+	return np.sum((x - 0.345) ** 2)
 
 def initialize_population(m, D, min_var, max_var):
-    """Initialize the population."""
-    pop = np.random.rand(m, D)
-    pop = pop * (max_var - min_var) + min_var
-    return pop
+	"""Initialize the population."""
+	pop = np.random.rand(m, D)
+	pop = pop * (max_var - min_var) + min_var
+	return pop
 
 def main():
-    # Parameters
-    n_swarm = 6
-    n_var = 1000
+	# Parameters
+	n_swarm = 6
+	n_var = 1000
 
-    min_var = -100 * np.ones(n_var)
-    max_var = 100 * np.ones(n_var)
+	min_var = -100 * np.ones(n_var)
+	max_var = 100 * np.ones(n_var)
 
-    w = 0.7298
-    c1 = 1.4961
-    c2 = 1.4961
+	w = 0.7298
+	c1 = 1.4961
+	c2 = 1.4961
 
-    # Initialize populations
-    pop = initialize_population(n_swarm, n_var, min_var, max_var)
-    pop_cost = np.array([np.append(p, sphere_function(p)) for p in pop])
-    
-    pop_cost = pop_cost[np.argsort(pop_cost[:, -1])]  # Sort by cost
-    g_best = pop_cost[0, :]
-    p_best = pop_cost.copy()
+	# Initialize populations
+	pop = initialize_population(n_swarm, n_var, min_var, max_var)
+	pop_cost = np.array([np.append(p, sphere_function(p)) for p in pop])
 
-    v = np.zeros((n_swarm, n_var))
-    
-    upper_v = 0.1 * max_var
-    lower_v = 0.1 * min_var
+	pop_cost = pop_cost[np.argsort(pop_cost[:, -1])]  # Sort by cost
+	g_best = pop_cost[0, :]
+	p_best = pop_cost.copy()
 
-    EFs = 0
-    T_h = 100
-    it1 = 0
+	v = np.zeros((n_swarm, n_var))
 
-    while EFs <= 3e6:
-        # Main loop
-        for i in range(n_swarm):
-            it = i - it1 * T_h
+	upper_v = 0.1 * max_var
+	lower_v = 0.1 * min_var
 
-            if it % T_h == 0:
-                loc = np.random.permutation(n_var)
-                loc1 = loc[:int(n_var * random.random() - 1) + 1]
-                Xs = np.random.permutation(n_swarm)
-                
-                pop[Xs[0], loc1] = np.random.rand(len(loc1)) * (max_var[loc1] - min_var[loc1]) + min_var[loc1]
-                Ccost = sphere_function(pop[Xs[0]])
-                
-                p_best[Xs[0]] = np.append(pop[Xs[0]], Ccost)
-                it1 += 1
-                EFs += 1
-            
-            two_pop = np.random.permutation(n_swarm)[:2]
+	EFs = 0
+	T_h = 100
+	it1 = 0
 
-            for j in two_pop:
-                v[j] = (
-                    c1 * random.random() * (p_best[j, :n_var] - pop_cost[j, :n_var])
-                    + c2 * random.random() * (g_best[:n_var] - pop_cost[j, :n_var])
-                    + w * v[j]
-                )
+	while EFs <= 3e6:
+		# Main loop
+		for i in range(n_swarm):
+			it = i - it1 * T_h
 
-                # Clipping velocity to upper_v and lower_v
-                v[j] = np.clip(v[j], lower_v, upper_v)
+			if it % T_h == 0:
+				loc = np.random.permutation(n_var)
+				loc1 = loc[:int(n_var * random.random() - 1) + 1]
+				Xs = np.random.permutation(n_swarm)
+				
+				pop[Xs[0], loc1] = np.random.rand(len(loc1)) * (max_var[loc1] - min_var[loc1]) + min_var[loc1]
+				Ccost = sphere_function(pop[Xs[0]])
+				
+				p_best[Xs[0]] = np.append(pop[Xs[0]], Ccost)
+				it1 += 1
+				EFs += 1
+			
+			two_pop = np.random.permutation(n_swarm)[:2]
 
-                # New position calculation
-                pop[j, :n_var] = pop_cost[j, :n_var] + v[j]
+			for j in two_pop:
+				v[j] = (
+					c1 * random.random() * (p_best[j, :n_var] - pop_cost[j, :n_var])
+					+ c2 * random.random() * (g_best[:n_var] - pop_cost[j, :n_var])
+					+ w * v[j]
+				)
 
-                # Boundary checks
-                pop[j, :n_var] = np.clip(pop[j, :n_var], min_var, max_var)
+				# Clipping velocity to upper_v and lower_v
+				v[j] = np.clip(v[j], lower_v, upper_v)
 
-                # Calculate new cost
-                CCcost = sphere_function(pop[j, :n_var])
+				# New position calculation
+				pop[j, :n_var] = pop_cost[j, :n_var] + v[j]
 
-                if CCcost <= pop_cost[j, -1]:
-                    pop_cost1[j] = np.append(pop[j, :n_var], CCcost)
-                else:
-                    pop_cost1[j] = pop_cost[j]
+				# Boundary checks
+				pop[j, :n_var] = np.clip(pop[j, :n_var], min_var, max_var)
 
-                EFs += 1
+				# Calculate new cost
+				CCcost = sphere_function(pop[j, :n_var])
+				pop_cost1 = np.array(pop_cost.copy())
+				if CCcost <= pop_cost[j, -1]:
+					pop_cost1[j] = np.append(pop[j, :n_var], CCcost)
+				else:
+					pop_cost1[j] = pop_cost[j]
 
-                # HBF strategy implementation
-                vx = np.random.randn(n_var)
-                beta = 0.5 + random.random()
-                stepsize = 0.00001 * (it / T_h) * ((1. / np.abs(vx)) ** (1 / beta)) * np.sign(vx)
-                Xs = p_best[j, :n_var] + (stepsize) * np.exp(it / 10)
-                Xa = p_best[j, :n_var] + 1 * (stepsize) * (1 - it / T_h) * (max_var - min_var)
+				EFs += 1
 
-                x = g_best[:n_var] if j < n_swarm else pop[j, :n_var]
+				# HBF strategy implementation
+				vx = np.random.randn(n_var)
+				beta = 0.5 + random.random()
+				stepsize = 0.00001 * (it / T_h) * ((1. / np.abs(vx)) ** (1 / beta)) * np.sign(vx)
+				Xs = p_best[j, :n_var] + (stepsize) * np.exp(it / 10)
+				Xa = p_best[j, :n_var] + 1 * (stepsize) * (1 - it / T_h) * (max_var - min_var)
 
-                H = np.abs(2 * random.random() * np.exp(2 - 2 * it / T_h) - np.exp(2 - 2 * it / T_h))
-                rr = np.random.rand(n_var)
-                cc = np.random.rand(n_var)
+				x = g_best[:n_var] if j < n_swarm else pop[j, :n_var]
 
-                xx = np.where(rr <= cc * it / T_h)[0]
+				H = np.abs(2 * random.random() * np.exp(2 - 2 * it / T_h) - np.exp(2 - 2 * it / T_h))
+				rr = np.random.rand(n_var)
+				cc = np.random.rand(n_var)
 
-                k1 = np.zeros(n_var)
-                k2 = np.zeros(n_var)
+				xx = np.where(rr <= cc * it / T_h)[0]
 
-                if H > 0.3 * random.random():
-                    k1[xx] = 1
-                else:
-                    k2[xx] = 1
+				k1 = np.zeros(n_var)
+				k2 = np.zeros(n_var)
 
-                k3 = np.zeros(n_var)
-                xx = np.where(rr > cc * it / T_h)[0]
-                k3[xx] = 1
+				if H > 0.3 * random.random():
+					k1[xx] = 1
+				else:
+					k2[xx] = 1
 
-                z = Xs * k1 + Xa * k2 + k3 * x
+				k3 = np.zeros(n_var)
+				xx = np.where(rr > cc * it / T_h)[0]
+				k3[xx] = 1
 
-                z = np.clip(z, min_var, max_var)
+				z = Xs * k1 + Xa * k2 + k3 * x
 
-                zcost = sphere_function(z)
-                EFs += 1
+				z = np.clip(z, min_var, max_var)
 
-                if zcost < p_best[j, -1]:
-                    p_best[j] = np.append(z, zcost)
+				zcost = sphere_function(z)
+				EFs += 1
 
-                if pop_cost1[j, -1] < p_best[j, -1]:
-                    p_best[j] = pop_cost1[j]
+				if zcost < p_best[j, -1]:
+					p_best[j] = np.append(z, zcost)
 
-        pop_cost = pop_cost1.copy()
-        pop_cost = pop_cost[np.argsort(pop_cost[:, -1])]
-        pop_cost_sort1 = p_best[np.argsort(p_best[:, -1])]
+				if pop_cost1[j, -1] < p_best[j, -1]:
+					p_best[j] = pop_cost1[j]
 
-        g_best1 = pop_cost_sort1[0]
+		pop_cost = pop_cost1.copy()
+		pop_cost = pop_cost[np.argsort(pop_cost[:, -1])]
+		pop_cost_sort1 = p_best[np.argsort(p_best[:, -1])]
 
-        if g_best1[-1] < g_best[-1]:
-            g_best = g_best1
+		g_best1 = pop_cost_sort1[0]
 
-        if i % 100 == 0:
-            print(f"EFs>> {EFs}: Best Cost = {g_best[-1]}")
+		if g_best1[-1] < g_best[-1]:
+			g_best = g_best1
+
+		if i % 100 == 0:
+			print(f"EFs>> {EFs}: Best Cost = {g_best[-1]}")
 
 if __name__ == "__main__":
-    main()
+	main()
--- main.py
+++ main.py
@@ -1,166 +1,192 @@
 import numpy as np
 import random
-import scipy.stats as stats
 
 
 def initialize_population(size, dim, X_min, X_max):
-    """Инициализация популяции частиц случайными значениями в заданном диапазоне."""
-    return np.random.uniform(X_min, X_max, (size, dim))
+	return np.random.uniform(X_min, X_max, (size, dim))
 
 
 def fitness_function(x):
-    """Пример функции пригодности для оценки качества решений."""
-    return np.sum(x ** 2)
+	return np.sum(x ** 2)
 
 
 def compute_velocity(particle, best_personal, best_global):
-    """Вычисление новой скорости частицы."""
-    inertia = 0.7  # Инерционная константа
-    c1, c2 = 2.05, 2.05  # Константы обучения
-    return (inertia * particle['velocity'] +
-            c1 * (best_personal - particle['position']) +
-            c2 * (best_global - particle['position']))
+	inertia = 0.7298  # Инерционная константа
+	c1, c2 = 1.4961, 1.4961  # Константы обучения
+	r1 = np.random.random()
+	r2 = np.random.random()
+	return (inertia * particle['velocity'] +
+			c1 * r1 * (best_personal - particle['position']) +
+			c2 * r2 * (best_global - particle['position']))
 
 
 def update_position(particle):
-    """Обновление позиции частицы."""
-    particle['position'] += particle['velocity']
+	particle['position'] += particle['velocity']
 
 
 def generate_theta(D):
-    # uniform_random_values = np.random.random(D)
-    # theta = stats.norm.ppf(uniform_random_values)
-    rndn_values = np.random.randn(D)
-    theta = 1 / rndn_values
-    return theta
+	theta = np.random.randn(D)
+	return theta
 
 
 def main():
-    dim = 10  # Размерность задачи оптимизации
-    X_min, X_max = -10, 10  # Диапазон значений переменных
-    population_size = 5  # Размер популяции
-    maxit = 10000  # Максимальное количество итераций
-    it = 0  # Текущая итерация
-    T = 100  # Время эволюции
-    t = 1  # Начальное значение t
-    A = np.random.random()  # Константа для выбора изменения параметра частицы
+	V_min, V_max = -10, 10  # Диапазон значений скорости
+	dim = 10  # Размерность задачи оптимизации
+	X_min, X_max = -100, 100  # Диапазон значений переменных
+	population_size = 4  # Размер популяции
+	maxit = 10000  # Максимальное количество итераций
+	n = 2 # Количество изменяемых частиц за итерацию
+	it = 0  # Текущая итерация
+	T = 100  # Момент времени запуска сценария смены позиции случайной частицы
+	t = 1  # Начальное значение t
+	A = np.random.random()  # Константа для выбора изменения параметра частицы
 
-    # Генерация начальной популяции X^{i,0} -- популяция i на итерации it
-    population_init = initialize_population(population_size, dim, X_min, X_max)
-    population = []
-    for i in range(population_size):
-        population.append({
-            'position': population_init[i],
-            'velocity': np.zeros(dim),
-            'best_position': population_init[i],
-            'best_fitness': fitness_function(population_init[i]),
-        })
+	# Генерация начальной популяции X^{i,0} -- популяция i на итерации it
+	population_init = initialize_population(population_size, dim, X_min, X_max)
+	population = []
+	for i in range(population_size):
+		population.append({
+			'position': population_init[i],
+			'velocity': np.zeros(dim),
+			'best_position': population_init[i],
+			'best_fitness': fitness_function(population_init[i]),
+		})
 
-    global_best = min(population, key=lambda p: p['best_fitness'])
-    global_best_position = global_best['best_position']
-    global_best_fitness = global_best['best_fitness']
+	global_best = min(population, key=lambda p: p['best_fitness'])
+	global_best_position = global_best['best_position']
+	global_best_fitness = global_best['best_fitness']
 
-    while it <= maxit:
+	while it <= maxit - n:
+		particles = random.sample(range(population_size), n)
 
-        # Выбор двух частиц для сравнения
-        r, k = random.sample(range(population_size), 2)
+		for p in particles:
+			old_particle = population[p]
+			new_particle = {
+				'position': old_particle['position'].copy(),
+				'velocity': old_particle['velocity'].copy(),
+				'best_position': old_particle['best_position'].copy(),
+				'best_fitness': old_particle['best_fitness'].copy(),
+			}
+			# Вычисление новой скорости для частицы p
+			velocity = compute_velocity(old_particle,
+										old_particle['best_position'],
+										global_best_position)
 
-        for i in [r, k]:
-            # Вычисление новой скорости для частицы i
-            velocity = compute_velocity(population[i],
-                                        population[i]['best_position'],
-                                        global_best_position)
+			# Проверка выхода значений скорости за границы
+			over_upper_velocity = velocity > V_max
+			for g in range(len(over_upper_velocity)):
+				if (over_upper_velocity[g]):
+					velocity[g] = np.random.uniform(V_min, V_max) 
+			over_lower_velocity = velocity < V_min
+			for g in range(len(over_lower_velocity)):
+				if (over_lower_velocity[g]):
+					velocity[g] = np.random.uniform(V_min, V_max) 
 
-            # Вычисляем вектор новой скорости
-            mask = np.random.choice([0, 1], size=len(velocity), p=[0.5, 0.5])
-            new_velocity = velocity * mask
-            # Устанавливаем новую скорость для новой частицы
+			# Вычисляем вектор новой скорости
+			mask = np.random.choice([0, 1], size=dim, p=[0.5, 0.5])
+			new_velocity = velocity * mask
 
-            population[i]['velocity'] = new_velocity
+			# Устанавливаем новую скорость для новой частицы
+			new_particle['velocity'] = new_velocity
 
-            # Обновление позиции
-            update_position(population[i])
+			# Обновление позиции
+			update_position(new_particle)
 
-            # Проверка на выход за пределы границ
-            population[i]['position'] = np.clip(population[i]['position'], X_min, X_max)
+			# Проверка на выход за пределы границ
+			new_particle['position'] = np.clip(new_particle['position'], X_min, X_max)
 
-            # Оценка новой позиции
-            new_fitness = fitness_function(population[i]['position'])
+			# Оценка новой позиции
+			new_fitness = fitness_function(new_particle['position'])
 
-            # Обновление частицы, если она лучше предыдущей
-            if new_fitness < population[i]['best_fitness']:
-                population[i]['best_position'] = population[i]['position'].copy()
-                population[i]['best_fitness'] = new_fitness
+			# обновление позиции и скорости старой частицы
+			population[p]['velocity'] = new_particle['velocity'].copy()
+			population[p]['position'] = new_particle['position'].copy()
 
-            # Обновление глобальной информации
-            if new_fitness < global_best_fitness:
-                global_best_position = population[i]['position'].copy()
-                global_best_fitness = new_fitness
+			# Обновление частицы, если она лучше предыдущей
+			if new_fitness < old_particle['best_fitness']:
+				population[p]['best_position'] = population[p]['position'].copy()
+				population[p]['best_fitness'] = new_fitness
 
-            # HBF process
-            # Расчёт шагов и коэффициентов корректировки
-            theta = generate_theta(dim)
-            beta = 0.5 + np.random.random()
-            adjustment_coefficient1 = np.exp(t / 10)
-            adjustment_coefficient2 = (1 - t / T) * (X_max - X_min)
-            step_size = (t / T) * 1e-5 * (1 / np.abs(np.abs(theta))) ** (1 / beta) * np.sign(theta)
+			# Обновление глобальной информации
+			if new_fitness < global_best_fitness:
+				global_best_position = population[p]['position'].copy()
+				global_best_fitness = new_fitness
 
-            X_hbf = population[i]['best_position'] + step_size * adjustment_coefficient1
-            Y_hbf = global_best_position + step_size * adjustment_coefficient2
-            Z_pso = population[i]
+			# HBF process
+			# Расчёт шагов и коэффициентов корректировки
+			theta = generate_theta(dim)
+			beta = 0.5 + np.random.random()
+			adjustment_coefficient1 = np.exp(t / 10)
+			adjustment_coefficient2 = (1 - t / T) * (X_max - X_min)
+			step_size = (t / T) * 1e-5 * (1 / np.abs(theta)) ** (1 / beta) * np.sign(theta)
 
-            h = np.exp(2 - 2 * t / T)
-            H = np.abs(2 * random.random() * h - h)
+			X_hbf = population[p]['best_position'] + step_size * adjustment_coefficient1
+			Y_hbf = global_best_position + step_size * adjustment_coefficient2
+			Z_pso = population[p]['position']
 
-            # Позиция для рассчитанной частицы
-            positions = []
+			h = np.exp(2 - 2 * t / T)
+			H = np.abs(2 * random.random() * h - h)
 
-            # Для каждого решения частицы
-            for j in range(dim):
-                rand1 = np.random.random()
-                rand2 = np.random.random()
-                rand3 = np.random.random()
-                rand4 = np.random.random()
-                # Различные случайные изменения
-                if rand1 <= rand2 + A * (t / T) + rand3:
-                    if H > rand4:
-                        positions.append(X_hbf[j])
-                    else:
-                        positions.append(Y_hbf[j])
-                else:
-                    positions.append(Z_pso['position'][j])
+			# Позиция для рассчитанной частицы
+			positions = []
 
-            # Проверяем на принадлежность области поиска
-            checked_positions = np.clip(positions, X_min, X_max)
+			# Для каждого решения частицы
+			for j in range(dim):
+				rand1 = np.random.random()
+				rand2 = np.random.random()
+				rand3 = np.random.random()
+				rand4 = np.random.random()
+				# Различные случайные изменения
+				if rand1 <= rand2 + A + rand3 * (t / T):
+					if H > rand4:
+						positions.append(X_hbf[j])
+					else:
+						positions.append(Y_hbf[j])
+				else:
+					positions.append(Z_pso[j])
 
-            # Оценка новой позиции
-            new_fitness = fitness_function(checked_positions)
+			# Проверяем на принадлежность области поиска
+			checked_positions = np.clip(positions, X_min, X_max)
 
-            # Обновление частицы, если она лучше предыдущей
-            if new_fitness < population[i]['best_fitness']:
-                population[i]['best_position'] = positions.copy()
-                population[i]['best_fitness'] = new_fitness
+			# Оценка новой позиции
+			new_fitness = fitness_function(checked_positions)
 
-            # Обновление глобальной информации
-            if new_fitness < global_best_fitness:
-                global_best_position = population[i]['position'].copy()
-                global_best_fitness = new_fitness
+			population[p]['position'] = checked_positions.copy()
 
-            # Обновление итерации
-            t += 1
-            it += 1
-            # Замена и корректировка по необходимости
-            if t == T:
-                t = 1
-                # m = random.choice(population)
-                # m['position'] = np.copy(global_best_position)
-                # mask = np.random.choice([0, 1], size=dim, p=[0.5, 0.5])
-                # ne
-                # m['best_position'] =
-                # m['best_fitness'] = global_best_fitness
+			# Обновление частицы, если она лучше предыдущей
+			if new_fitness < population[p]['best_fitness']:
+				population[p]['best_position'] = checked_positions.copy()
+				population[p]['best_fitness'] = new_fitness
 
-        print(f"Итерация {it}: Лучшее значение = {global_best_fitness}")
+			# Обновление глобальной информации
+			if new_fitness < global_best_fitness:
+				global_best_position = population[p]['position'].copy()
+				global_best_fitness = new_fitness
 
+			# Обновление итерации
+			t += 1
+			it += 1
+			
+			# Замена и корректировка по необходимости
+			if t == T:
+				t = 1
+				m = random.choice(population)
+				new_position = []
+				random_position = initialize_population(1, dim, X_min, X_max)[0]
+				mask = np.random.choice([0, 1], size=dim, p=[0.5, 0.5])
+				for i in range(len(mask)):
+					if mask[i] == 1:
+						new_position.append(global_best_position[i])
+					else:
+						new_position.append(random_position[i])
+				new_position = np.array(new_position).copy()
+				new_fitness = fitness_function(new_position)
+				if (new_fitness < fitness_function(m['position'])):
+					m['position'] = new_position.copy()
+					m['best_position'] = np.array(new_position).copy()
+					m['best_fitness'] = new_fitness
+
+			print(f"Итерация {it}: Лучшее значение = {global_best_fitness}")
 
 if __name__ == "__main__":
     main()
